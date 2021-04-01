fb53163efab9b8aa8888b9aa6e14815d78c7d6b4
luq11Z <lucas_segundinho@hotmail.com>
2021-04-01 11:38:24 +0100

    Project created

---
+++ apicrudclients/src/main/java/com/lucaslearning/apicrudclients/ApicrudclientsApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.apicrudclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicrudclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicrudclientsApplication.class, args);
+	}
+
+}
