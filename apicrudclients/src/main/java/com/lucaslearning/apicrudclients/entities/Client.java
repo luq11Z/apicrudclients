9012db0f0e4608101233bcbf91aa4ef7f369f138
luq11Z <lucas_segundinho@hotmail.com>
2021-04-01 12:38:05 +0100

    h2 database, sql insert, jpa mapping

--- apicrudclients/src/main/java/com/lucaslearning/apicrudclients/entities/Client.java
+++ apicrudclients/src/main/java/com/lucaslearning/apicrudclients/entities/Client.java
@@ -3,16 +3,37 @@ package com.lucaslearning.apicrudclients.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
-public class Client implements Serializable{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	public Client() {
 
@@ -73,6 +94,24 @@ public class Client implements Serializable{
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
---
+++ apicrudclients/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Lucas Silva', 81645372502, 1500.0, TIMESTAMP WITH TIME ZONE '1995-12-18T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Cristina Baptista', 77644185798, 1500.0, TIMESTAMP WITH TIME ZONE '1993-06-20T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Alex Green', 55462296592, 600.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Bob Grey', 78377834928, 850.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Maria Brown', 63916010891, 900.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Arthur Yellow', 22289863564, 1500.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Max Red', 82780300493, 1500.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Nelio Alves', 49424581459, 9999999.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Ellen Pink', 62209184525, 1500.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) 
+VALUES ('Christian Darkgrey', 23381277308, 1500.0, TIMESTAMP WITH TIME ZONE '1990-09-27T10:00:00Z', 0, NOW());
