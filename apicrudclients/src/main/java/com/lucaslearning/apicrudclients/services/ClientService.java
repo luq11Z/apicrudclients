48dc5d8fa69bf09c8e70a41d951983093f8e48f4
luq11Z <lucas_segundinho@hotmail.com>
2021-04-03 09:13:29 +0100

    Client delete

--- apicrudclients/src/main/java/com/lucaslearning/apicrudclients/services/ClientService.java
+++ apicrudclients/src/main/java/com/lucaslearning/apicrudclients/services/ClientService.java
@@ -2,7 +2,10 @@ package com.lucaslearning.apicrudclients.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,29 +18,49 @@ import com.lucaslearning.apicrudclients.services.exceptions.ObjectNotFoundExcept
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		copyEntityToDto(entity, dto);
 		repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyEntityToDto(entity, dto);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Id not found " + id);
+		}
 	}
 
 	private void copyEntityToDto(Client entity, ClientDTO dto) {
